1169b56869968f7f0f718a459d7afe12ca21b91b
Bishal1991 <bk9506346@gmail.com>
2026-09-25 00:51:27 +0545

    signing up buttons

--- lib/SignUpPage.dart
+++ lib/SignUpPage.dart
@@ -19,75 +19,122 @@ class Signuppage extends StatelessWidget {
               icon: const Icon(Icons.close),
             ),
           ),
+          Container(
+            margin: EdgeInsets.only(top: 90),
+            child: Center(
 
-          Center(
-
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "First Name",
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                      child: Text("Sign Up page",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "First Name",
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top:10),
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Last Name",
+                  Container(
+                    margin: EdgeInsets.only(top:10),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Last Name",
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "email",
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "email",
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "username",
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "username",
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Password",
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Password",
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top:10),
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Confirm Password",
+                  Container(
+                    margin: EdgeInsets.only(top:10),
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Confirm Password",
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(onPressed: () {}, child: Text("Login" ))
-              ],
+                  Container(
+                    margin: EdgeInsets.only(top: 20, bottom: 10),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        ),
+                        textStyle: TextStyle(fontSize: 20),
+                        elevation: 10,
+                        foregroundColor: Colors.white70,
+                        backgroundColor: Colors.black,
+                        ),
+                        onPressed: () {},
+                        child: Text("Create Account" )),
+                  ),
+                  Text("or Continue with"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        width: 50,
+                        decoration: BoxDecoration(color: Colors.white),
+                        child:  Image.network(
+                            'http://pngimg.com/uploads/google/google_PNG19635.png',
+                            fit:BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: 40,
+                        decoration: BoxDecoration(color: Colors.white),
+                        child:  Image.network(
+                          'https://static.freepnglogo.com/images/all_img/facebook-logo.png',
+                          fit:BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
